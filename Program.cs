88d5909d29c907297c43a8f6edf6b112eded9f9f
unknown <bd7q9887u@code.edu.az>
2023-09-28 11:32:17 +0400

    task1

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Bir tam sayı girin: ");
+int n = int.Parse(Console.ReadLine());
+
+bool powerTwo = false;
+if (n>0)
+{
+    if ((n&(n-1))==0)
+    {
+        powerTwo = true;
+    }
+}
+if (powerTwo)
+{
+    Console.WriteLine("2 nin quvvetidir");
+}
+else
+{
+    Console.WriteLine("2 nin quvveti deyil");
+}
+
+
